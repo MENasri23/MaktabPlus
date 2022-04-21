837c6323eec479853b619e9e6bf7e6c8d5b26124
MENasri23 <m.e26159487@gmail.com>
2022-04-21 14:20:43 +0430

    Move MainActivity to ui package

--- app/src/main/java/com/example/maktabplus/ui/MainActivity.kt
+++ app/src/main/java/com/example/maktabplus/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.maktabplus
+package com.example.maktabplus.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.maktabplus.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
